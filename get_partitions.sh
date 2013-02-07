c3442ee65543184019bcdab0be69e3a81f715c66
lzq55555 <15168341@qq.com>
2013-02-07 16:34:11 +0800

    1

--- get_partitions.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p /sdcard/partitions
-for l in `cat partitions_links`
-do
-   file=`echo $l | cut -d':' -f1`
-   blk=`echo $l | cut -d':' -f2`
-   blkname=`echo $blk | cut -d'/' -f4`
-   echo $file @ $blk
-   cat $blk > /sdcard/partitions/$file@$blkname.img
-done
